9970994879b7f419f09f5e3c841d7900ae4a3966
matis <98737015+madis2@users.noreply.github.com>
2022-09-05 17:17:31 +0300

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+package harjutused_matis;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
+
---
+++ muutujad_matis.java
@@ -0,0 +1,41 @@
+package harjutused_matis;
+
+public class muutujad_matis {
+
+	public static void main(String[] args) {
+		String eesnimi = new String();
+		eesnimi = "Matis";
+		int kursus = 2;
+		
+		System.out.println(eesnimi+", "+kursus+". kursus");
+		System.out.println();
+		
+		/* Täisarv vanuse jaoks */
+		
+		int vanus = 17;
+		
+		/* Komaarv enda pikkuse jaoks */
+		
+		float komaarv = 1.78f;
+		
+		/* String hüüdnime jaoks */
+		
+		String hyydnimi = new String();
+		hyydnimi = "Madis";
+		
+		/* Täislause kasutades muutujaid */
+		
+		System.out.println("Olen "+vanus+" aastat vana, "+komaarv+"m pikk ning huudnimeks on "+hyydnimi+".");
+		
+		/*
+		
+		int minuarv;
+		
+		System.out.println(minuarv);
+		
+		Üleval olev annab välja errori.
+		
+		*/
+	}
+
+}
