d9269d9432cfb793346eded7f83b9fd1b326d9c1
matis <98737015+madis2@users.noreply.github.com>
2022-09-08 10:48:38 +0300

    Update dünaamilineloend_matis.java

--- dünaamilineloend_matis.java
+++ dünaamilineloend_matis.java
@@ -3,19 +3,43 @@
 package harjutused_matis;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.*;
+import java.io.PrintWriter;
 import java.io.*;
 
 public class dünaamilineloend_matis {
 
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList <Integer>();
+		Scanner scanner = new Scanner (System.in);
 		
-		int kordus = 0;
+		int kordus = 1;
 		
-		while(kordus == 0) {
-			Scanner scanner = new Scanner (System.in);
+		while(kordus == 1) {
 			System.out.print("Sisesta arv: ");
-			int arv = scanner.nextInt();
+			String arv = scanner.nextLine();
+			
+			if(arv == "") {
+				break;
+			}
+		
+			int number = Integer.parseInt(arv);
+			list.add(number);
+		}
+		int kokku = list.size();
+		int sum = list.stream().mapToInt(Integer::intValue).sum();
+		int keskmine = sum/kokku;
+		
+
+		try {
+			PrintWriter bw = new PrintWriter("numbrid.txt");
+			bw.println("Kõik arvud: "+ list);
+			bw.println("Summa: "+sum);
+			bw.println("Keskmine: "+ keskmine);
+			bw.close();
+			System.out.println("Fail loodud!");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
