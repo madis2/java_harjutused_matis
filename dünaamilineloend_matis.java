0fafe02efbab1ca911e8c32c81334f4fac1a9c97
matis <98737015+madis2@users.noreply.github.com>
2022-09-08 09:29:12 +0300

    Add files via upload

---
+++ dünaamilineloend_matis.java
@@ -0,0 +1,21 @@
+/* Matis Russi IT-21 */
+
+package harjutused_matis;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+public class dünaamilineloend_matis {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList <Integer>();
+		
+		int kordus = 0;
+		
+		while(kordus == 0) {
+			Scanner scanner = new Scanner (System.in);
+			System.out.print("Sisesta arv: ");
+			int arv = scanner.nextInt();
+		}
+	}
+}
